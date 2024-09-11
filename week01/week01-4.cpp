cdb3980e0d5a666c60b8c6058bdb741a89301fe7
shinying0419 <lishinying05@gmail.com>
2024-09-11 11:32:53 +0800

    week01

---
+++ week01/week01-4.cpp
@@ -0,0 +1,13 @@
+//week01-4.cpp CC17-09A
+//Not OK. But easy for you
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	cout << n/4 %2;
+	cout << n/2 %2;
+	cout << n%2;
+	return 0;
+}
