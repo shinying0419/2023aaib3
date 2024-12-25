a9ce96419d9f5c741467be27f52eae993ac2247d
shinying0419 <lishinying05@gmail.com>
2024-12-25 11:09:59 +0800

    week16

---
+++ week16/week16-5.cpp
@@ -0,0 +1,21 @@
+//week16-5.cpp YKL33.UVA299 Train Swapping 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int T,N;
+	cin >> T;
+	for(int t=0;t<T;t++){
+		cin>>N;
+		vector<int> a(N);
+		for(int i=0;i<N;i++){
+			cin>>a[i];
+			cout << a[i]<<" ";
+		}
+		cout << "\n";
+		
+		cout << "Optimal train swapping take swaps.\n";
+	}
+}
